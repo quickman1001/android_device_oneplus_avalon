6ebaec8badba7c6fd3db4a98330cac1ae0df5904
Mashopy <eliasgheeraert@gmail.com>
2026-04-19 12:07:23 +0000

    avalon: Enable remapping AOSP effect IDs to RichTap's internal effect ID
    Change-Id: I7b9a25129984c0c51d4851290825871520adbb83
    Signed-off-by: K R I S H N A <puresoulkk.github@proton.me>

--- device.mk
+++ device.mk
@@ -68,6 +68,7 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 PRODUCT_PACKAGES += \
     android.hardware.vibrator.service.oplus-richtap
 
+$(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USE_RICHTAP_EFFECT_REMAP,true)
 $(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USES_OPLUS_AWINIC,true)
 
 # ViPER4Android FX
